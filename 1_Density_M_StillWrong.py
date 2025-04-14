50fafea3779487522343c3fabf071780e56b8341
Alireza-Lashkaripour <alireza.lashkaripour00@gmail.com>
2025-04-14 10:44:33 +0900

    new things

--- 1_Density_M_StillWrong.py
+++ 1_Density_M_StillWrong.py
@@ -2,9 +2,8 @@ import numpy as np
 np.random.seed(42)
 from qiskit import QuantumCircuit, transpile
 from qiskit.circuit.library import UnitaryGate
-from qiskit.transpiler import CouplingMap
 from qiskit_aer import AerSimulator
-from qiskit.quantum_info import DensityMatrix, SparsePauliOp, Statevector
+from qiskit.quantum_info import DensityMatrix, SparsePauliOp
 from qiskit_nature.second_q.formats.fcidump import FCIDump
 from qiskit_nature.second_q.formats.fcidump_translator import fcidump_to_problem
 from qiskit_nature.second_q.mappers import JordanWignerMapper
@@ -17,14 +16,8 @@ def complete_unitary_from_fixed_row(v):
     X[:, 0] = v.conj()
     Q, _ = np.linalg.qr(X)
     phase = np.vdot(Q[:, 0], v.conj())
-    if abs(phase) < 1e-12:
-        phase = 1.0
     Q[:, 0] *= phase.conjugate() / abs(phase)
-    U = Q.conj().T
-    u, s, vh = np.linalg.svd(U)
-    U_unitary = u @ vh
-    return U_unitary
-
+    return Q.conj().T
 
 def complete_unitary_from_fixed_rows(M):
     m, n = M.shape
@@ -33,19 +26,14 @@ def complete_unitary_from_fixed_rows(M):
     Q, _ = np.linalg.qr(X)
     for j in range(m):
         phase = np.vdot(Q[:, j], M[j, :].conj())
-        if abs(phase) < 1e-12:
-            phase = 1.0
         Q[:, j] *= phase.conjugate() / abs(phase)
-    U = Q.conj().T
-    u, s, vh = np.linalg.svd(U)
-    U_unitary = u @ vh
-    return U_unitary
+    return Q.conj().T
 
-
-def complete_gate_for_site(tensor, site_type, target_dim):
+def complete_gate_for_site(tensor, site_type):
+    d = 2
     if site_type == "first":
         if tensor.ndim < 2:
-            return np.eye(target_dim * target_dim, dtype=complex)
+            return np.eye(d*d, dtype=complex)
         v = tensor.reshape(-1)
         norm = np.linalg.norm(v)
         if norm:
@@ -53,10 +41,9 @@ def complete_gate_for_site(tensor, site_type, target_dim):
         return complete_unitary_from_fixed_row(v)
     elif site_type == "intermediate":
         if tensor.ndim < 3:
-            return np.eye(target_dim * target_dim, dtype=complex)
-        # Create M with shape (target_dim, target_dim^2)
-        M = np.zeros((target_dim, target_dim * target_dim), dtype=complex)
-        for j in range(target_dim):
+            return np.eye(d*d, dtype=complex)
+        M = np.zeros((d, d*d), dtype=complex)
+        for j in range(d):
             vec = tensor[j, :, :].reshape(-1)
             norm = np.linalg.norm(vec)
             if norm:
@@ -71,7 +58,7 @@ def complete_gate_for_site(tensor, site_type, target_dim):
             v /= norm
         return complete_unitary_from_fixed_row(v)
     else:
-        return np.eye(target_dim * target_dim, dtype=complex)
+        return np.eye(d*d, dtype=complex)
 
 def pick_largest_block(tensor, qlabels):
     if isinstance(tensor, np.ndarray):
@@ -118,12 +105,11 @@ def truncate_to_target(tensor, site_type, target=2):
     else:
         return tensor
 
-def build_deep_circuit_iterative(mps_data, target_dim=2, layers=1):
+def build_deep_circuit_iterative(mps_data, target_dim=2, layers=3):
+    # Make a deep copy of mps_data so we can update its tensors iteratively.
     mps_updated = {key: mps_data[key] for key in mps_data}
     n_sites = mps_updated['n_sites']
-    # Total qubits: first site uses 2, each intermediate uses 4, last uses 1.
-    total_qubits = 2 + 4 * (n_sites - 2) + 1
-    full_qc = QuantumCircuit(total_qubits)
+    full_qc = QuantumCircuit(n_sites)
     for L in range(layers):
         dense_tensors = []
         for i in range(n_sites):
@@ -131,76 +117,54 @@ def build_deep_circuit_iterative(mps_data, target_dim=2, layers=1):
             q = mps_updated['q_labels'][i]
             block = pick_largest_block(t, q)
             if i == 0:
-                dense = truncate_to_target(block, "first", target=2)
+                dense = truncate_to_target(block, "first", target_dim)
             elif i == n_sites - 1:
-                dense = truncate_to_target(block, "last", target=2)
+                dense = truncate_to_target(block, "last", target_dim)
             else:
-                dense = truncate_to_target(block, "intermediate", target=target_dim)
+                dense = truncate_to_target(block, "intermediate", target_dim)
             dense_tensors.append(dense)
         U_list = []
         if len(dense_tensors) == 1:
-            U = complete_gate_for_site(dense_tensors[0], "first", target_dim=2)
+            U = complete_gate_for_site(dense_tensors[0], "first")
             U_list.append(U)
         else:
-            U_list.append(complete_gate_for_site(dense_tensors[0], "first", target_dim=2))
+            U_list.append(complete_gate_for_site(dense_tensors[0], "first"))
             for i in range(1, n_sites - 1):
-                U_list.append(complete_gate_for_site(dense_tensors[i], "intermediate", target_dim))
-            U_list.append(complete_gate_for_site(dense_tensors[-1], "last", target_dim=2))
-        # Build layer circuit with proper qubit assignments:
-        layer_qc = QuantumCircuit(total_qubits)
-        # First gate on qubits [0,1]:
-        first_gate_qubits = [0, 1]
-        layer_qc.append(UnitaryGate(U_list[0].conj().T, label="U1_layer"+str(L)), first_gate_qubits)
-        # Intermediate gates: assign 4 qubits per gate.
+                U_list.append(complete_gate_for_site(dense_tensors[i], "intermediate"))
+            U_list.append(complete_gate_for_site(dense_tensors[-1], "last"))
+        layer_qc = QuantumCircuit(n_sites)
+        layer_qc.append(UnitaryGate(U_list[0].conj().T, label="U1_layer"+str(L)), [0, 1])
         for i in range(1, n_sites - 1):
-            start_index = 2 + 4 * (i - 1)
-            gate_qubits = list(range(start_index, start_index + 4))
-            layer_qc.append(UnitaryGate(U_list[i].conj().T, label="U" + str(i+1) + "_layer" + str(L)), gate_qubits)
-        # Last gate on the final qubit:
-        last_gate_qubits = [total_qubits - 1]
-        layer_qc.append(UnitaryGate(U_list[-1].conj().T, label="U" + str(n_sites) + "_layer" + str(L)), last_gate_qubits)
+            layer_qc.append(UnitaryGate(U_list[i].conj().T, label="U"+str(i+1)+"_layer"+str(L)), [i, i+1])
+        layer_qc.append(UnitaryGate(U_list[-1].conj().T, label="U"+str(n_sites)+"_layer"+str(L)), [n_sites-1])
         full_qc = full_qc.compose(layer_qc)
-        # Optional: iterative update (if needed)
+        # Iterative update (crude): replace current tensors with the dense ones
         mps_updated["tensors"] = dense_tensors
     full_qc.save_density_matrix(label="rho")
     return full_qc
 
-
-
-qc = build_deep_circuit_iterative(mps_data, target_dim=2, layers=1)
+qc = build_deep_circuit_iterative(mps_data, target_dim=2, layers=3)
 print(qc.draw(output="text", reverse_bits=True))
 
-sim = AerSimulator(method="statevector")
-coupling_map = CouplingMap.from_full(qc.num_qubits)
-compiled = transpile(qc, sim, coupling_map=coupling_map,
-                     optimization_level=0, layout_method="trivial")
+sim = AerSimulator(method="density_matrix")
+compiled = transpile(qc, sim)
 job = sim.run(compiled, shots=1)
-result = sim.run(compiled, shots=1).result()
-
-
-
-state = result.get_statevector(qc)
-sv = Statevector(state)
+result = job.result()
+data = result.data(0)
+print(data)
+print("Result data keys:", list(data.keys()))
 
 fcidump = FCIDump.from_file("H2O.STO3G.FCIDUMP")
 problem = fcidump_to_problem(fcidump)
 mapper = JordanWignerMapper()
-#qubit_op = mapper.map(problem.second_q_ops()[0])
-qubit_op = mapper.map(problem.hamiltonian)
+qubit_op = mapper.map(problem.second_q_ops()[0])
 
-
-H_mat = qubit_op.to_matrix()
-num_hamiltonian_qubits = int(np.log2(H_mat.shape[0]))
-print("Hamiltonian acts on", num_hamiltonian_qubits, "qubits.")
-print("Circuit has", qc.num_qubits, "qubits.")
-
-if qc.num_qubits > num_hamiltonian_qubits:
-    extra_qubits = list(range(num_hamiltonian_qubits, qc.num_qubits))
-    print("Tracing out qubits:", extra_qubits)
-    rho_reduced = sv.partial_trace(extra_qubits)
-else:
-    from qiskit.quantum_info import DensityMatrix
-    rho_reduced = DensityMatrix(sv)
-
-energy_circ = np.real(np.trace(H_mat @ rho_reduced.data))
+rho_mat = DensityMatrix(data["rho"])
+def pad_density(rho, n_missing):
+    zero_proj = np.zeros((2**n_missing, 2**n_missing))
+    zero_proj[0,0] = 1.0
+    return np.kron(rho, zero_proj)
+rho_padded = pad_density(rho_mat.data, 7)
+energy_circ = np.real(np.trace(qubit_op.to_matrix() @ rho_padded))
 print("Energy from circuit:", energy_circ)
+
--- GIVES_CLOSE_RESLUT.py
+++ GIVES_CLOSE_RESLUT.py
@@ -135,82 +135,70 @@ def truncate_to_target(tensor, site_type, target=4):
         return tensor
 
 
-def build_deep_circuit_iterative(mps_data, target_dim=4, layers=3):
+def build_layer(mps_data, target_dim=4, layer_idx=0):
+    mps_updated = {key: mps_data[key] for key in mps_data}
+    n_sites = mps_updated['n_sites']
+    total_qubits = 2 + 4 * (n_sites - 2) + 1
+    dense_tensors = []
+    for i in range(n_sites):
+        t = mps_updated['tensors'][i]
+        q = mps_updated['q_labels'][i]
+        block = pick_largest_block(t, q)
+        if i == 0:
+            dense = truncate_to_target(block, "first", target=2)
+        elif i == n_sites - 1:
+            dense = truncate_to_target(block, "last", target=2)
+        else:
+            dense = truncate_to_target(block, "intermediate", target=target_dim)
+        dense_tensors.append(dense)
+    U_list = []
+    if len(dense_tensors) == 1:
+        U = complete_gate_for_site(dense_tensors[0], "first", target_dim=2)
+        U_list.append(U)
+    else:
+        U_list.append(complete_gate_for_site(dense_tensors[0], "first", target_dim=2))
+        for i in range(1, n_sites - 1):
+            U_list.append(complete_gate_for_site(dense_tensors[i], "intermediate", target_dim))
+        U_list.append(complete_gate_for_site(dense_tensors[-1], "last", target_dim=2))
+    layer_qc = QuantumCircuit(total_qubits)
+    first_gate_qubits = [0, 1]
+    layer_qc.append(UnitaryGate(U_list[0].conj().T, label="U1_layer"+str(layer_idx)), first_gate_qubits)
+    for i in range(1, n_sites - 1):
+        start_index = 2 + 4 * (i - 1)
+        gate_qubits = list(range(start_index, start_index + 4))
+        layer_qc.append(UnitaryGate(U_list[i].conj().T, label="U"+str(i+1)+"_layer"+str(layer_idx)), gate_qubits)
+    last_gate_qubits = [total_qubits - 1]
+    layer_qc.append(UnitaryGate(U_list[-1].conj().T, label="U"+str(n_sites)+"_layer"+str(layer_idx)), last_gate_qubits)
+    return layer_qc, dense_tensors
+
+
+def simulate_layers(mps_data, target_dim=4, layers=3):
     mps_updated = {key: mps_data[key] for key in mps_data}
     n_sites = mps_updated['n_sites']
     total_qubits = 2 + 4 * (n_sites - 2) + 1
     full_qc = QuantumCircuit(total_qubits)
     for L in range(layers):
-        dense_tensors = []
-        for i in range(n_sites):
-            t = mps_updated['tensors'][i]
-            q = mps_updated['q_labels'][i]
-            block = pick_largest_block(t, q)
-            if i == 0:
-                dense = truncate_to_target(block, "first", target=2)
-            elif i == n_sites - 1:
-                dense = truncate_to_target(block, "last", target=2)
-            else:
-                dense = truncate_to_target(block, "intermediate", target=target_dim)
-            dense_tensors.append(dense)
-        U_list = []
-        if len(dense_tensors) == 1:
-            U = complete_gate_for_site(dense_tensors[0], "first", target_dim=2)
-            U_list.append(U)
-        else:
-            U_list.append(complete_gate_for_site(dense_tensors[0], "first", target_dim=2))
-            for i in range(1, n_sites - 1):
-                U_list.append(complete_gate_for_site(dense_tensors[i], "intermediate", target_dim))
-            U_list.append(complete_gate_for_site(dense_tensors[-1], "last", target_dim=2))
-        layer_qc = QuantumCircuit(total_qubits)
-        first_gate_qubits = [0, 1]
-        layer_qc.append(UnitaryGate(U_list[0].conj().T, label="U1_layer"+str(L)), first_gate_qubits)
-        for i in range(1, n_sites - 1):
-            start_index = 2 + 4 * (i - 1)
-            gate_qubits = list(range(start_index, start_index + 4))
-            layer_qc.append(UnitaryGate(U_list[i].conj().T, label="U"+str(i+1)+"_layer"+str(L)), gate_qubits)
-        last_gate_qubits = [total_qubits - 1]
-        layer_qc.append(UnitaryGate(U_list[-1].conj().T, label="U"+str(n_sites)+"_layer"+str(L)), last_gate_qubits)
+        layer_qc, dense_tensors = build_layer(mps_updated, target_dim, layer_idx=L)
         full_qc = full_qc.compose(layer_qc)
         mps_updated["tensors"] = dense_tensors
-    full_qc.save_density_matrix(label="rho")
     return full_qc
 
-qc = build_deep_circuit_iterative(mps_data, target_dim=4, layers=3)
-print(qc.draw(output="text", reverse_bits=True))
+final_circuit = simulate_layers(mps_data, target_dim=4, layers=3)
+print(final_circuit.draw(output="text", reverse_bits=True))
+
+
+fcidump = FCIDump.from_file("H2O.STO3G.FCIDUMP")
+problem = fcidump_to_problem(fcidump)
+mapper = JordanWignerMapper()
+qubit_op = mapper.map(problem.second_q_ops()[0])
+
+final_circuit.save_expectation_value(qubit_op, list(range(14)), label="exp_val")
 
 sim = AerSimulator(method="matrix_product_state", max_parallel_threads=1, max_parallel_experiments=1)
-
-coupling_map = CouplingMap.from_full(qc.num_qubits)
-compiled = transpile(qc, sim, coupling_map=coupling_map, optimization_level=0, layout_method="trivial")
-
+coupling_map = CouplingMap.from_full(final_circuit.num_qubits)
+compiled = transpile(final_circuit, sim, coupling_map=coupling_map, optimization_level=0, layout_method="trivial")
 job = sim.run(compiled, shots=1)
 result = job.result()
-
-dm_data = result.data(0).get("rho")
-if dm_data is None:
-    print("No density matrix saved; simulation may have encountered an error.")
-else:
-    rho = DensityMatrix(dm_data)
-    print("Density Matrix from circuit:")
-    print(rho.data)
-
-    fcidump = FCIDump.from_file("H2O.STO3G.FCIDUMP")
-    problem = fcidump_to_problem(fcidump)
-    mapper = JordanWignerMapper()
-    qubit_op = mapper.map(problem.hamiltonian)
-    H_mat = qubit_op.to_matrix()
-    num_hamiltonian_qubits = int(np.log2(H_mat.shape[0]))
-    print("Hamiltonian acts on", num_hamiltonian_qubits, "qubits.")
-    print("Circuit has", qc.num_qubits, "qubits.")
-
-    if qc.num_qubits > num_hamiltonian_qubits:
-        extra_qubits = list(range(num_hamiltonian_qubits, qc.num_qubits))
-        print("Tracing out qubits:", extra_qubits)
-        dm_reduced = partial_trace(rho, extra_qubits)
-    else:
-        dm_reduced = rho
-
-    energy_circ = float(np.real(np.trace(H_mat @ dm_reduced.data)))
-    print("Energy from circuit:", energy_circ)
+energy_circ = result.data(0)["exp_val"]
+print("Energy from circuit:", energy_circ)
 
